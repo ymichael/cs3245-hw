592b85e894a69e7f746a313958e3cba78f1e42b8
Michael Yong <wrong92@gmail.com>
2014-02-28 23:03:33 +0800

    Add from json and to json methods for dictionary.

--- dictionary.py
+++ dictionary.py
@@ -1,17 +1,19 @@
 import collections
+import json
 
 
 class Dictionary(object):
     def __init__(self):
         self.terms = collections.defaultdict(DocumentIdLinkedList)
-
-    def number_of_docs(self, term):
-        return self.terms[term].length
+        self.doc_ids = []
 
     def has_entry(self, term, doc_id):
         return self.terms[term].has_entry(doc_id)
 
     def add_term(self, term, doc_id, pointer):
+        if doc_id not in self.doc_ids:
+            self.doc_ids.append(doc_id)
+
         self.terms[term].add_doc(doc_id, pointer)
 
     def get_frequency(self, term):
@@ -26,10 +28,43 @@ class Dictionary(object):
     def all_terms(self):
         return self.terms.keys()
 
+    def all_docs(self):
+        return sorted(self.doc_ids)
+
+    def to_json(self):
+        terms = {}
+        for term in self.terms.keys():
+            terms[term] = {
+                'frequency': self.get_frequency(term),
+                'head': self.get_head(term),
+                'tail': self.get_tail(term),
+            }
+
+        dict_repr = {
+            'doc_ids': self.all_docs(),
+            'terms': terms
+        }
+
+        return json.dumps(dict_repr)
+
+    @staticmethod
+    def from_json(json_repr):
+        dict_repr = json.loads(json_repr)
+
+        d = Dictionary()
+        d.doc_ids = dict_repr['doc_ids']
+        for term, val in dict_repr['terms'].iteritems():
+            d.terms[term].head = val['head']
+            d.terms[term].tail = val['tail']
+            d.terms[term].set_frequency(val['frequency'])
+
+        return d
+
 
 class DocumentIdLinkedList(object):
     def __init__(self):
         self.doc_ids = []
+        self._frequency = None
         self._head = None
         self._tail = None
 
@@ -44,14 +79,28 @@ class DocumentIdLinkedList(object):
             self.doc_ids.append(doc_id)
             self._tail = pointer
 
+    def set_frequency(self, frequency):
+        self._frequency = frequency
+
     @property
     def length(self):
+        if self._frequency is not None:
+            return self._frequency
+
         return len(self.doc_ids)
 
     @property
     def head(self):
         return self._head
 
+    @head.setter
+    def head(self, head):
+        self._head = head
+
     @property
     def tail(self):
         return self._tail
+
+    @tail.setter
+    def tail(self, tail):
+        self._tail = tail
--- test_dictionary.py
+++ test_dictionary.py
@@ -2,15 +2,6 @@ from dictionary import Dictionary
 from nose.tools import eq_ as assert_eq
 
 
-def test_dictionary_number_of_docs():
-    d = Dictionary()
-    assert_eq(0, d.number_of_docs('asdf'))
-
-    d.add_term('asdf', 1, 10)
-    assert d.number_of_docs('asdf')
-    assert_eq(0, d.number_of_docs('qwer'))
-
-
 def test_dictionary_has_entry():
     d = Dictionary()
     assert not d.has_entry('asdf', 1)
@@ -63,3 +54,42 @@ def test_dictionary_all_terms():
     assert_eq(
         sorted(['asdf', 'qwer', 'zxcv']),
         sorted(d.all_terms()))
+
+
+def test_dictionary_all_docs():
+    d = Dictionary()
+    assert_eq([], d.all_docs())
+
+    d.add_term('asdf', 1, 1)
+    assert_eq([1], d.all_docs())
+
+    d.add_term('asdf', 2, 1)
+    assert_eq([1, 2], d.all_docs())
+
+    d.add_term('qwer', 1, 1)
+    d.add_term('zxcv', 1, 1)
+    assert_eq([1, 2], d.all_docs())
+
+
+def test_dictionary_to_json_from_json():
+    d = Dictionary()
+    d.add_term('asdf', 1, 1)
+    d.add_term('asdf', 2, 1)
+    d.add_term('qwer', 1, 1)
+    d.add_term('zxcv', 1, 1)
+
+    d2 = Dictionary.from_json(d.to_json())
+    assert_eq(d2.all_docs(), d.all_docs())
+    assert_eq(d2.all_terms(), d.all_terms())
+
+    assert_eq(d2.get_frequency('asdf'), d.get_frequency('asdf'))
+    assert_eq(d2.get_frequency('qwer'), d.get_frequency('qwer'))
+    assert_eq(d2.get_frequency('zxcv'), d.get_frequency('zxcv'))
+
+    assert_eq(d2.get_head('asdf'), d.get_head('asdf'))
+    assert_eq(d2.get_head('qwer'), d.get_head('qwer'))
+    assert_eq(d2.get_head('zxcv'), d.get_head('zxcv'))
+
+    assert_eq(d2.get_tail('asdf'), d.get_tail('asdf'))
+    assert_eq(d2.get_tail('qwer'), d.get_tail('qwer'))
+    assert_eq(d2.get_tail('zxcv'), d.get_tail('zxcv'))
